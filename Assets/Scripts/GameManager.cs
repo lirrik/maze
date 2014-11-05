639838677c9a56536cf0ebaa7c2a33aef40fa680
Kirill Basin <kir.basin@gmail.com>
2014-11-05 15:23:55 +1100

    Added player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-
     public Maze mazePrefab;
     private Maze mazeInstance;
+    public Player playerPrefab;    
+    private Player playerInstance;
 
     private void Start()
     {
-        BeginGame();
+        StartCoroutine(BeginGame());
     }
     
     private void Update()
@@ -20,16 +21,22 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void BeginGame()
+    private IEnumerator BeginGame()
     {
         mazeInstance = Instantiate(mazePrefab) as Maze;
-        StartCoroutine(mazeInstance.Generate());
+        yield return StartCoroutine(mazeInstance.Generate());
+        playerInstance = Instantiate(playerPrefab) as Player;
+        playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
     }
-
+    
     private void RestartGame()
     {
         StopAllCoroutines();
         Destroy(mazeInstance.gameObject);
-        BeginGame();
+        if (playerInstance != null)
+        {
+            Destroy(playerInstance.gameObject);
+        }
+        StartCoroutine(BeginGame());
     }
 }
